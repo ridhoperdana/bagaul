8fe4136f6807fd2ea67ddb4efe30d7e5a21080a4
ridhoperdana <ridhoperdana@ymail.com>
2021-02-20 18:16:19 +0700

    feat: init bagaul

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express'),
+    app = express(),
+    cors = require('cors'),
+    bodyParser = require('body-parser');
+
+app.use(cors())
+app.use(bodyParser.json());
+
+app.post("/convert", async function (req, res) {
+    const requestedText = req.body["text"];
+    const langType = req.body["type"];
+
+    let newText = ""
+
+    switch (langType) {
+        case "g":
+            for (var i = 0; i < requestedText.length; i++) {
+                newText += requestedText.charAt(i);
+                if (requestedText.charAt(i) == "a" || requestedText.charAt(i) == "i" || requestedText.charAt(i) == "u" || requestedText.charAt(i) == "e" || requestedText.charAt(i) == "o") {
+                    newText += "g";
+                    newText += requestedText.charAt(i);
+                }
+            }
+            break;
+
+        default:
+            res.statusCode = 400;
+            res.send({
+                "error": "type not supported"
+            });
+            return;
+            break;
+    }
+
+
+    res.statusCode = 200;
+    res.send({
+        "result": newText
+    });
+});
+
+app.listen("3000", () => {
+    console.log('App started');
+});
